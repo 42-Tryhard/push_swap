6684f059bd49931d4250ca75c7f15d5b4c64f0c1
olivier349 <olivierliove@gmail.com>
2023-04-30 18:32:31 +0200

    MAJ

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: oliove <olivierliove@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 08:12:27 by oliove            #+#    #+#             */
-/*   Updated: 2023/03/24 16:12:01 by oliove           ###   ########.fr       */
+/*   Updated: 2023/03/24 20:39:22 by oliove           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int main(int ac, char **av)
     
     int tableau[] = {3,1,5,6,7,2,9,4,8,0};
     int tab[] = {12,16,18,20,21,22,23,24,28,26};
+
 	int lenght = sizeof(tableau) / sizeof(int);
   
     i = 0;
